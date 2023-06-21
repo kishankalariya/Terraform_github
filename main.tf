d2b8feaa29c90a3199f757197e27c6c90de73137
kishankalariya <kishankalariya199@gmail.com>
2023-06-21 15:16:33 +0100

    v1.1 - terraform file added

--- main.tf
+++ main.tf
@@ -19,20 +19,20 @@ provider "google" {
 
 }
 
-resource "google_compute_instance" "terraform-test" {
+resource "google_compute_instance" "terraform-test-v1" {
 
-  name         = "terraform-test"
+  name         = "terraform-test-v1"
   machine_type = "e2-medium"
   zone = "europe-west2-c"
 
 
   boot_disk {
     auto_delete = true
-    device_name = "terraform-test"
+    device_name = "terraform-test-v1"
 
     initialize_params {
       image = "projects/debian-cloud/global/images/debian-11-bullseye-v20230615"
-      size  = 11
+      size  = 10
       type  = "pd-balanced"
     }
 
