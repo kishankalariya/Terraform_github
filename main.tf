b2912620a68311f2cafb834847347d01ac80c97c
kishankalariya <kishankalariya199@gmail.com>
2023-06-23 16:36:14 +0100

    check test 2

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "google_compute_instance" "terraform-test-v2" {
 
     initialize_params {
       image = "projects/debian-cloud/global/images/debian-11-bullseye-v20230615"
-      size  = 10
+      size  = 11
       type  = "pd-balanced"
     }
 
