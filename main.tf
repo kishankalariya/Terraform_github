006c4c5c663e4dd57a0549afec5d340e48f68945
kishankalariya <kishankalariya199@gmail.com>
2023-06-27 15:25:49 +0100

    normal test on feature branch

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 
 
-resource "google_compute_instance" "terraform-test-v9" {
+resource "google_compute_instance" "terraform-test-v4" {
 
-  name         = "terraform-test-v9"
+  name         = "terraform-test-v4"
   machine_type = "e2-medium"
   zone         = "europe-west2-c"
 
 
   boot_disk {
     auto_delete = true
-    device_name = "terraform-test-v9"
+    device_name = "terraform-test-v4"
 
     initialize_params {
       image = "projects/debian-cloud/global/images/debian-11-bullseye-v20230615"
