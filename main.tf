bd0569314d2015d58eb6784669707b8ef27a6fc3
kishankalariya <kishankalariya199@gmail.com>
2023-07-18 17:55:54 +0100

    test 1

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 
 
-resource "google_compute_instance" "terraform-test-v6" {
+resource "google_compute_instance" "terraform-test-v1" {
 
-  name         = "terraform-test-v6"
+  name         = "terraform-test-v1"
   machine_type = "e2-medium"
   zone         = "europe-west2-c"
 
 
   boot_disk {
     auto_delete = true
-    device_name = "terraform-test-v6"
+    device_name = "terraform-test-v1"
 
     initialize_params {
       image = "projects/debian-cloud/global/images/debian-11-bullseye-v20230615"
